e0edeb0a87c83dde2d58355ff1afbfa918bbaa6e
Shade <shadework@outlook.com>
2021-07-29 17:42:03 +0800

    test git

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Sky } from "drei";
+import { Canvas } from "react-three-fiber";
+import { Vector3 } from "three";
 
 
 function App() {
   return (
-    <>
-    </>
+    <Canvas>
+      <Sky sunPosition={new Vector3(100, 10, 100)} />
+    </Canvas>
   );
 }
 
